9aa87af0708d6cbf41af08ad970332418cf68b68
Mario Viapiano <marioeviapiano@gmail.com>
2014-12-21 20:25:31 -0300

    Add test to grid elements

--- spec/vida/grid_spec.rb
+++ spec/vida/grid_spec.rb
@@ -1,25 +1,42 @@
 require_relative '../../lib/vida/grid'
 
-describe Grid do
+describe Vida::Grid do
+  let(:grid) { Vida::Grid.new }
+
   it "should respond to rows" do
-    expect(Grid.new.respond_to?(:rows)).to eq true
+    expect(grid.respond_to?(:rows)).to eq true
   end
   it "should respond to columns" do
-    expect(Grid.new.respond_to?(:columns)).to eq true
+    expect(grid.respond_to?(:columns)).to eq true
   end
+  it "should respond to elements" do
+    expect(grid.respond_to?(:elements)).to eq true
+  end
+
   describe ".initialize method" do
     context 'with arguments' do
       it "creates a grid with the given size" do
-        grid = Grid.new(rows: 10, columns: 15)
-        expect(grid.rows).to eq 10
-        expect(grid.columns).to eq 15
+        grid_wa = Vida::Grid.new(rows: 10, columns: 15)
+        expect(grid_wa.rows).to eq 10
+        expect(grid_wa.columns).to eq 15
       end
     end
     context 'without arguments' do
       it "creates a grid with default values" do
-        grid = Grid.new
         expect(grid.rows).to eq 50
         expect(grid.columns).to eq 50
+      end
+    end
+  end
+
+  describe "Grid elements" do
+    it "should be a matrix of Vida::Cell objects" do
+      expect(grid.elements.is_a?(Array)).to be true
+      grid.elements.each do |column|
+        expect(column.is_a?(Array)).to be true
+        column.each do |element|
+          expect(element.is_a?(Vida::Cell))
+        end
       end
     end
   end
