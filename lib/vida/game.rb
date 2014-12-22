51f7a5faf345eb1616f2fa63efd6728b0c9ebec6
Mario Viapiano <marioeviapiano@gmail.com>
2014-12-22 02:19:48 -0300

    Add play method in Game class.

--- lib/vida/game.rb
+++ lib/vida/game.rb
@@ -1,6 +1,15 @@
 class Vida::Game
+  attr_reader :grid
 
   def initialize(grid)
     @grid = grid
+  end
+
+  def play
+    loop do
+      next_generation
+      system('clear')
+      puts grid
+    end
   end
 end
